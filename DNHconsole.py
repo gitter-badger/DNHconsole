5abb32f23aba1536527efe61d8bc5b3a25b8b26f
thanhmssl10 <thanhmssl10@gmail.com>
2015-05-12 00:34:37 +0700

    Hoan thien tong the lan 1

---
+++ DNHconsole.py
@@ -0,0 +1,245 @@
+#Ham chuyen tu co dau sang khong dau UTF-8
+#Date first code: 2015/5/11
+#Tac gia: Thanh Pham - @thanhmssl10 - DNH
+#Input la link raw cua Daynhauhoc.com, Output la text da duoc bo dau
+
+domain='http://daynhauhoc.com'
+GioiHanDong=999
+WaitString="\nCho chut nhe :D ......"
+
+LinkTrangHome=[]
+TenBaiVietGlobal=[]
+for i in range(0,50):       
+        LinkTrangHome.append("")
+        TenBaiVietGlobal.append("")
+
+def see(ID):
+    if LinkTrangHome[ID]=="":
+        home()
+    show(LinkTrangHome[ID],ID)
+    
+def showall(link):
+    print "\n\n\n"
+    print "===================================================="
+    print "===================================================="
+    print "===================================================="
+    print "\n\n\n"
+    print "\n"
+    print sourcetext(link)
+    print "\n\n\n"
+    print "====================================================" 
+    print "===================================================="
+    print "====================================================" 
+    print "\n\n\n"
+def show(link,ID):
+    print WaitString
+    
+    import urllib2
+    web = urllib2.urlopen(link)
+    tep=open('tempDNHpage.txt',"w")
+    tep.write(web.read())
+    tep.close()
+   
+    import unicodedata
+    input = open('tempDNHpage.txt').read().decode('UTF-8')
+    output = unicodedata.normalize('NFKD', input).encode('ASCII', 'ignore')
+
+    tep=open('tempDNHpage.txt',"w")
+    tep.write(output)
+    tep.close()
+ 
+    tep=open('tempDNHpage.txt',"r")
+    
+    print "\n\n\n"
+    print "===================================================="
+    print "===================================================="
+    print "===================================================="
+    print "\n\n\n"
+    print "  "+"["+str(ID)+"]:"+TenBaiVietGlobal[ID]
+    print "\n"
+    for i in range(1,GioiHanDong):
+        if tep.readline()!="":
+            print tep.readline()
+            raw_input()
+    print "\n\n\n"
+    print "====================================================" 
+    print "===================================================="
+    print "====================================================" 
+    print "\n\n\n"
+    tep.close()
+def sourcetext(link):
+    print WaitString
+    
+    import urllib2
+    web = urllib2.urlopen(link)
+    tep=open('tempDNHpage.txt',"w")
+    tep.write(web.read())
+    tep.close()
+   
+    import unicodedata
+    input = open('tempDNHpage.txt').read().decode('UTF-8')
+    output = unicodedata.normalize('NFKD', input).encode('ASCII', 'ignore')
+
+    tep=open('tempDNHpage.txt',"w")
+    tep.write(output)
+    tep.close()
+    return output
+
+def home():
+    TempHome=open('tempHome.txt','w')
+    TempHome.write(sourcetext(domain))
+    TempHome.close()
+    
+    TempHome=open('tempHome.txt','r')
+    lineData=[]
+    for i in range(0,GioiHanDong+1):
+        lineData.append(TempHome.readline())
+
+
+
+    
+    STTTopic=0
+    for i in range(0,GioiHanDong):
+        TenBaiViet=LayChuoi(lineData[i],"<span itemprop='name'>","<")
+        if (TenBaiViet!=-1) and (STTTopic < 20):
+            STTTopic = STTTopic+1
+            if STTTopic <10:
+                print "\n[0"+str(STTTopic)+"]: "+ str(TenBaiViet)
+                
+            else:
+                print "\n["+str(STTTopic)+"]: "+ str(TenBaiViet)
+            TenBaiVietGlobal[STTTopic]=TenBaiViet
+
+    STTLink=0
+    for i in range(0,GioiHanDong):
+        LinkBaiViet=LayChuoi(lineData[i],"<meta itemprop='url' content='","'")
+        if (LinkBaiViet!=-1) and (STTLink < 20):
+            STTLink= STTLink+1
+            LinkTrangHome[STTLink]=linktoraw(LinkBaiViet)
+               
+                
+def linktoraw(link):
+    ViTriCuaKiTuBatDau=link.find("/",len(domain)+5)
+
+    DemSoKiTuCanCat=0
+    for i in range(0,200):
+        try:
+            if link[ViTriCuaKiTuBatDau:][i].isdigit:
+                DemSoKiTuCanCat+=1
+            else:
+                break
+        except:
+            break
+            pass
+        
+        
+    linkRaw=link[ViTriCuaKiTuBatDau+1:ViTriCuaKiTuBatDau+DemSoKiTuCanCat]
+    return domain+"/raw/" + str(linkRaw)
+    
+def LayChuoi(DayGoc,DayDinhVi,KiTuKetThuc):
+
+    ViTriCuaDayDinhVi=DayGoc.find(DayDinhVi)
+    ViTriCuaKiTuKetThuc=0
+    if ViTriCuaDayDinhVi>0:
+        ViTriCuaKiTuKetThuc=DayGoc.find(KiTuKetThuc,(ViTriCuaDayDinhVi+len(DayDinhVi)))
+
+    DayOutput=""
+    if ViTriCuaKiTuKetThuc>0:
+        DayOutput=DayGoc[(ViTriCuaDayDinhVi+len(DayDinhVi)):ViTriCuaKiTuKetThuc]
+
+
+    if DayOutput != "":
+        return DayOutput
+    else:
+        return -1
+def help():
+    print """
+
+
+
+
+
+
+    #Gioi thieu chung cac chuc nang cua DNHconsole
+
+    Day la module truy cap dien dan Daynhauhoc.com qua giao dien dong lenh
+    (console) duoc viet bang Python 2.7.9 do Thanh Pham - @thanhmssl10 dat
+    nhung dong code dau tien vao ngay 11/5/2015.
+
+    ##Moudule co cac ham chinh nhu sau:
+
+    ###2 Ham User su dung nhieu nhat    
+    home():Ham nay khong co tham so, dung de xem cac bai viet moi nhat
+    --------------------------------------------------------------------
+    see(ID):   Co 1 tham so truyen vao la so tu 1 den 20
+    --------------------------------------------------------------------
+
+
+    ###Cac ham mo rong    
+    show():    Co 2 tham so truyen vao la Link cua trang can xem va ID cua no
+    vidu:      show('http://daynhauhoc.com/raw/7127',1)
+
+    Ham nay in ra tren man hinh noi dung cua trang web va chuyen dong khi an
+    Enter
+    --------------------------------------------------------------------
+
+
+    showall():  Co 1 tham so truyen vao la Link cua trang can xem
+    vidu:       show('http://daynhauhoc.com/raw/7127',1)
+    ...
+    Ham day se in toan bo noi dung cua trang web ra cung luc
+    --------------------------------------------------------------------
+    
+
+
+
+
+
+    Enter de doc tiep...
+    """
+    raw_input()
+    print """
+
+
+
+
+
+
+    #Gioi thieu chung cac chuc nang cua DNHconsole
+
+    Ngoai nhung ham do ra thi module con co mot ho ham khac de phuc vu cho
+    viec trich xuat du lieu tu website.
+
+    Cac ham do la nhung ham tim, cat chuoi, nhung ham de chuyen tu link thuong
+    sang link raw
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+    
+    """
